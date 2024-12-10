c1902d47def4e03e77d3734dfd9f49015ce3235e
alejandroacevedo2305 <a.acevedo.aracena@gmail.com>
2024-12-10 13:08:00 -0300

    deploy ignored

--- lgraph_essentials/parallelization.py
+++ lgraph_essentials/parallelization.py
@@ -1,1 +1,123 @@
 # %%
+from lgraph_essentials.llm import llm
+from IPython.display import Image, display
+
+from langchain_core.messages import SystemMessage, HumanMessage, RemoveMessage
+from langchain_core.runnables import RunnableConfig
+
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph import MessagesState
+
+from IPython.display import Image, display
+
+from typing import Any
+from typing_extensions import TypedDict
+
+import operator
+from typing import Annotated
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import HumanMessage, SystemMessage
+
+from langchain_community.document_loaders import WikipediaLoader
+from langchain_community.tools import TavilySearchResults
+
+# from langchain_community.tools.pubmed.tool import PubmedQueryRun
+
+from langchain_community.tools.semanticscholar.tool import SemanticScholarQueryRun
+from langchain.retrievers import PubMedRetriever
+from langchain_community.utilities.pubmed import PubMedAPIWrapper
+
+retriever = PubMedRetriever()
+resultados = retriever.invoke("flux balance analysis")
+pubmed = PubMedAPIWrapper()
+resultados = pubmed.run("flux balance analysis")
+
+sc = SemanticScholarQueryRun()
+sc.invoke("flux balance analysis")
+
+
+# %%
+class State(TypedDict):
+    question: str
+    answer: str
+    context: Annotated[list, operator.add]
+
+
+def search_web(state):
+    """Retrieve docs from web search"""
+
+    # Search
+    tavily_search = TavilySearchResults(max_results=3)
+    search_docs = tavily_search.invoke(state["question"])
+
+    # Format
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document href="{doc["url"]}"/>\n{doc["content"]}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+
+    return {"context": [formatted_search_docs]}
+
+
+def search_wikipedia(state):
+    """Retrieve docs from wikipedia"""
+
+    # Search
+    search_docs = WikipediaLoader(query=state["question"], load_max_docs=2).load()
+
+    # Format
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document source="{doc.metadata["source"]}" page="{doc.metadata.get("page", "")}"/>\n{doc.page_content}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+
+    return {"context": [formatted_search_docs]}
+
+
+def generate_answer(state):
+    """Node to answer a question"""
+
+    # Get state
+    context = state["context"]
+    question = state["question"]
+
+    # Template
+    answer_template = """Answer the question {question} using this context: {context}"""
+    answer_instructions = answer_template.format(question=question, context=context)
+
+    # Answer
+    answer = llm.invoke(
+        [SystemMessage(content=answer_instructions)]
+        + [HumanMessage(content="Answer the question.")]
+    )
+
+    # Append it to state
+    return {"answer": answer}
+
+
+# Add nodes
+builder = StateGraph(State)
+
+# Initialize each node with node_secret
+builder.add_node("search_web", search_web)
+builder.add_node("search_wikipedia", search_wikipedia)
+builder.add_node("generate_answer", generate_answer)
+
+# Flow
+builder.add_edge(START, "search_wikipedia")
+builder.add_edge(START, "search_web")
+builder.add_edge("search_wikipedia", "generate_answer")
+builder.add_edge("search_web", "generate_answer")
+builder.add_edge("generate_answer", END)
+graph = builder.compile()
+
+display(Image(graph.get_graph().draw_mermaid_png()))
+# %%
+
+result = graph.invoke({"question": "que es flux balance analysis"})
+result["answer"].content
